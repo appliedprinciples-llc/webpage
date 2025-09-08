4cb8629c06dd76a5000ade07b9724e7b8341ebcf
Matthew Carlson <mcarlson.ucsb@gmail.com>
2025-09-08 11:17:37 -0700

    fix warnings

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/', // use this for custom url in Github Pages
+    base: '/', // use this for custom url in GitHub Pages
 });
